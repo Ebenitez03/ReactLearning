ec0bd928d2f6623092c1b47531f5402464de66cd
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-01-03 07:11:53 +0100

    refactor: fixed the conditions

--- src/componentes/Welcome.jsx
+++ src/componentes/Welcome.jsx
@@ -4,11 +4,11 @@ export function Welcome({ name = "Jimmy", age }) {
   return (
     <div>
         <p>Welcome, <strong>{name}</strong></p>
-        {age && <Age age={age}/>}
         {!!age && <Age age={age}/>}
+        {age && <Age age={age}/>}
         {age>=18 && <Age age={age}/>}
-        {65<=age>=18 && <Age age={age}/>}
-        {65<=age>=18 && name=='John' && <Age age={age}/>}
+        {age<=65 && age>=18 && <Age age={age}/>}
+        {age<=65 && age>=18 && name=='John' && <Age age={age}/>}
     </div>
   );
 }
