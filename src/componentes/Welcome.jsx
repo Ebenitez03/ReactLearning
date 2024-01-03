4cd27061915be9e47ba46cc5c1ff6092f12a04c3
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-01-03 06:59:18 +0100

    feat: modified welcome component to render age component conditionally

--- src/componentes/Welcome.jsx
+++ src/componentes/Welcome.jsx
@@ -3,8 +3,12 @@ import Age from "./Age";
 export function Welcome({ name = "Jimmy", age }) {
   return (
     <div>
-      <p>Welcome, <strong>{name}</strong></p>
-    <Age age={age}/>
+        <p>Welcome, <strong>{name}</strong></p>
+        {age && <Age age={age}/>}
+        {!!age && <Age age={age}/>}
+        {age>=18 && <Age age={age}/>}
+        {65<=age>=18 && <Age age={age}/>}
+        {65<=age>=18 && name=='John' && <Age age={age}/>}
     </div>
   );
 }
