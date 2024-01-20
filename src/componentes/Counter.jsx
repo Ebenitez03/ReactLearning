e874d241a80fbde1f99e486f3b88ef5eee26f193
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-01-20 11:52:11 +0100

    refactor: reorganized buttons

--- src/componentes/Counter.jsx
+++ src/componentes/Counter.jsx
@@ -1,26 +1,27 @@
 import CounterDisplay from "./CounterDisplay";
 import { useState } from "react";
 
-
-export function Counter({initialValue=0,sum=1, rest=1}){
-    const [counter, setCounter]= useState(initialValue);
-    function handleSum(){
-        setCounter((c)=>c+sum);
-    }
-    function handleRest(){
-        setCounter((c)=>c-rest)
-    }
-    function handleReset(){
-        setCounter(initialValue);
-    }
-    return(
-        <div>
-            <CounterDisplay counter={counter}/>
-            <button onClick={handleSum}>Sum</button>
-            <button onClick={handleRest}>Rest</button>
-            <button onClick={handleReset}>Reset</button>
-        </div>
-    );
+export function Counter({ initialValue = 0, sum = 1, rest = 1 }) {
+  const [counter, setCounter] = useState(initialValue);
+  function handleSum() {
+    setCounter((c) => c + sum);
+  }
+  function handleRest() {
+    setCounter((c) => c - rest);
+  }
+  function handleReset() {
+    setCounter(initialValue);
+  }
+  return (
+    <div>
+      <CounterDisplay counter={counter} />
+      <div>
+        <button onClick={handleSum}>Sum</button>
+        <button onClick={handleRest}>Rest</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
 }
 export default Counter;
 
