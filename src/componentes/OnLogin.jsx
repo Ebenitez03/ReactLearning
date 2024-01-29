0e644563fc468b4861f6ba09c2f29ab4d5216bbb
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-01-29 16:06:32 +0100

    refactor:added form tag and onSubmit event

--- src/componentes/OnLogin.jsx
+++ src/componentes/OnLogin.jsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 export function OnLogin({name="", password=""}) {
     console.log(name)
     const [submit, setSubmit]= useState(false);
-    function handleSubmit(){
+    function handleSubmit(event){
+      event.preventDefault();
         setSubmit(true);
     }
   return (
-    <div>
-    <button disabled={!name||!password} onClick={handleSubmit}>Submit</button>
+    <form onSubmit={handleSubmit}>
+    <button type="submit" disabled={!name||!password}>Submit</button>
       {submit&&<p>{name} is logged</p>}
-    </div>
+    </form>
   );
 }
 export default OnLogin;
+
+//We use event.preventDefault to prevent default
