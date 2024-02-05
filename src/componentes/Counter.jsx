44e464299c38a12a1a8a23cb0a0849fa3c086b2a
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-02-05 16:19:30 +0100

    refactor: added direction ref and set it to log only when different

--- src/componentes/Counter.jsx
+++ src/componentes/Counter.jsx
@@ -1,26 +1,40 @@
 import CounterDisplay from "./CounterDisplay";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export function Counter({ initialValue = 0, sum = 1, rest = 1 }) {
   const [counter, setCounter] = useState(initialValue);
+  const [prevDirection, setPrevDirection] = useState('');
+  const mountedRef = useRef(false);
+  const direction = useRef(null);
   function handleSum() {
     setCounter((c) => c + sum);
+    direction.current = "up";
   }
   function handleRest() {
     setCounter((c) => c - rest);
+    direction.current = "down";
   }
   function handleReset() {
     setCounter(initialValue);
   }
-  useEffect(()=>{
-        console.log(`The counter it's currently ${counter}`)
-  }, [counter])
+  useEffect(() => {
+    console.log(`The counter it's currently ${counter}`);
+    if (direction.current !== prevDirection) {
+      console.log(direction.current);
+      setPrevDirection(direction.current);
+    }
+  }, [counter]);
+
   return (
     <div>
       <CounterDisplay counter={counter} />
       <div>
-        <button onClick={handleSum}>Sum</button>
-        <button onClick={handleRest}>Rest</button>
+        <button ref={direction} onClick={handleSum}>
+          Sum
+        </button>
+        <button ref={direction} onClick={handleRest}>
+          Rest
+        </button>
         <button onClick={handleReset}>Reset</button>
       </div>
     </div>
