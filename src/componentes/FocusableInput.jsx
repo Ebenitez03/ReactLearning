d8ee3d09b91dc51f030c7dffe96d57234eddfa85
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-02-05 13:08:25 +0100

    refactor: console logs different messagges when mounted first time and so on

--- src/componentes/FocusableInput.jsx
+++ src/componentes/FocusableInput.jsx
@@ -1,15 +1,22 @@
 import { useEffect } from "react";
 import { useRef } from "react";
 
-export function FocusableInput(){
-    const focusableInput=useRef(null);
-    useEffect(()=>{
-        focusableInput.current?.focus();
-    },[]);
-    return(
-        <div>
-            <input ref={focusableInput} placeholder="This input is focused"/>
-        </div>
-    )
+export function FocusableInput() {
+  const mountedRef = useRef(false);
+  const focusableInput = useRef(null);
+  useEffect(() => {
+    if (!mountedRef.current) {
+      mountedRef.current=true;
+      console.log("First time mounted");
+    } else {
+      console.log("Else");
+    }
+    focusableInput.current?.focus();
+  }, []);
+  return (
+    <div>
+      <input ref={focusableInput} placeholder="This input is focused" />
+    </div>
+  );
 }
 export default FocusableInput;
