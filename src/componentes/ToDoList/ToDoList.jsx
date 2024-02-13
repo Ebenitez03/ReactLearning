9c4c790c8f74ee635de5b40e09ef073fbb383b75
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-02-13 15:34:03 +0100

    refactor: imported styles

---
+++ src/componentes/ToDoList/ToDoList.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import styles from "./ToDoList.module.scss";
+
+export function ToDoList() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
+
+  function handleChange(event) {
+    setToDo(event.currentTarget.value);
+  }
+
+  function handleSubmit() {
+    setToDos((prevToDos) => [...prevToDos, toDo]);
+    setToDo("");
+  }
+
+  function handleReset() {
+    setToDos([]);
+  }
+
+  function handleRemove(index) {
+    setToDos((prevToDos) => prevToDos.filter((_, i) => i !== index));
+  }
+
+  return (
+    <div >
+      <input id="toDo" value={toDo} onChange={handleChange} type="text" />
+      <button onClick={handleSubmit} type="submit">
+        Añade un ToDo
+      </button>
+      <button onClick={handleReset}>Borra la lista</button>
+      <ul>
+        {toDos.map((toDo, index) => (
+          <li key={index}>
+            {toDo}
+            <button onClick={() => handleRemove(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
