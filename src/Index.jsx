856d588cdff91819330e67c92ae6b603c8220cd7
Ebenitez03 <elianenbenitezd04@gmail.com>
2024-02-13 13:53:16 +0100

    refactor: imported styles file

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import styles from "./styles/index.module.scss";
 
 createRoot(document.getElementById("root")).render(<App />);
